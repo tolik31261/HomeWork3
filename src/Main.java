8b70e57046d2d90faaabf7355a65b5ac5752c7c3
User <nazarovtolya2@gmail.com>
2023-02-04 21:04:43 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,18 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
+        byte a = 127;
+        System.out.println("Значение переменной a, " + "c типом byte, равно: " + a );
+        short b = 32767;
+        System.out.println("Значение переменной b, " + "c типом short, равно: " + b );
+        int c = 2147483647;
+        System.out.println("Значение переменной c, " + "c типом int, равно: " + c );
+        long d = 12021065025312L;
+        System.out.println("Значение переменной d, " + "c типом long, равно: " + d );
+        float e = 3.1f;
+        System.out.println("Значение переменной e, " + "c типом float, равно: " + e );
+        double f = 1.32489723498230;
+        System.out.println("Значение переменной f, " + "c типом double, равно: " + f );
     }
 
     public static void task2() {
